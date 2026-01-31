fc4a0101c53d65477f3b9de2560a25681abd2db7
bwinklesky <46791574+bwinklesky@users.noreply.github.com>
2026-01-31 09:20:45 -0700

    update

--- src/qs/MapboxMauiQs/MauiProgram.cs
+++ src/qs/MapboxMauiQs/MauiProgram.cs
@@ -5,6 +5,7 @@ namespace MapboxMauiQs;
 
 public static partial class MauiProgram
 {
+	public static string ACCESS_TOKEN = "pk.eyJ1IjoiYndpbmtsZXNreSIsImEiOiJjanlveGg0M2gwMnN1M21xdHVrcDMxeHluIn0.v7h89PF-OFLLQxD_sRling";
     public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
